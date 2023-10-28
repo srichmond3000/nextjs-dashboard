e147d8430124a599d427894512333efad097d427
Steve Richmond <srichmond3000@googlemail.com>
2023-10-28 14:48:23 +0100

    Complete chapter 3 fonts and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image';
 import AcmeLogo from '@/app/ui/acme-logo';
+import { lusitana } from './ui/fonts';
 
 import Link from 'next/link';
 export default function Page() {
@@ -12,7 +14,7 @@ export default function Page() {
           <div
             className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
           />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -27,7 +29,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
